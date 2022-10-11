abd392980b8bc75551069058b86189f6cb3a547a
mubarizahmed <mebza@live.com>
2022-10-11 22:43:09 +0200

    Updated sketches

--- code/arduino-vehicle-mubariz/arduino-vehicle-mubariz.ino
+++ code/arduino-vehicle-mubariz/arduino-vehicle-mubariz.ino
@@ -2,8 +2,10 @@
 #include <NewPing.h>      // Include Newping Library
 
 // L298N Control Pins
+const int LeftMotorPWM = 6;
 const int LeftMotorForward = 7;
 const int LeftMotorBackward = 5;
+const int RightMotorPWM = 3;
 const int RightMotorForward = 2;
 const int RightMotorBackward = 4;
 
@@ -20,12 +22,16 @@ int distance = 100;
 enum Motor { LEFT, RIGHT };
 
 // Set motor speed: 255 full ahead, −255 full reverse , 0 stop
-void go( enum Motor m, int speed)
-{
-digitalWrite (m == LEFT ? in1Pin : in3Pin, speed > 0 ? HIGH : LOW );
-digitalWrite (m == LEFT ? in2Pin : in4Pin, speed <= 0 ? HIGH : LOW );
-analogWrite(m == LEFT ? enAPin : enBPin, speed < 0 ? −speed : speed );
-}
+void go( enum Motor m, int speed){
+  digitalWrite (m == LEFT ? LeftMotorForward : RightMotorForward, speed > 0 ? HIGH : LOW );
+  digitalWrite (m == LEFT ? LeftMotorBackward : RightMotorBackward, speed <= 0 ? HIGH : LOW );
+  analogWrite(m == LEFT ? LeftMotorPWM : RightMotorPWM, speed < 0 ? -speed : speed );
+};
+
+void reverse(){
+  go(LEFT, -255);
+  go(RIGHT, -255);
+};
 
 int readPing()      // Read Ping Function for Ultrasonic Sensor.
 {
@@ -35,7 +41,7 @@ int readPing()      // Read Ping Function for Ultrasonic Sensor.
     cm=250;
   }
   return cm;
-}
+};
 
 void setup()
 {
@@ -44,6 +50,8 @@ void setup()
   pinMode(LeftMotorForward, OUTPUT);
   pinMode(LeftMotorBackward, OUTPUT);
   pinMode(RightMotorBackward, OUTPUT);
+  pinMode(LeftMotorPWM,OUTPUT);
+  pinMode(RightMotorPWM,OUTPUT);    
   
   servo_motor.attach(11);   // Attachs the servo on pin 9 to servo object.
   servo_motor.write(90);   // Set at 115 degrees. 
@@ -56,20 +64,48 @@ void setup()
   delay(100);
   distance = readPing();
   delay(100);
-}
+};
+
 
 void loop()
 {  
-  int angle = {0,30,60,90,120,150,180};
+  int angle[] = {0,30,60,90,120,150,180};
   int farthest_dist = 0;
   int farthest_angle = 90;
 
-  for (int i = 0; i < 7; i++){
+  for (int i = 0; i < 8; i++){
+    servo_motor.write(angle[i]);
     int dist = readPing();
-        
+    delay(200);
+    if (dist > farthest_dist) {
+      farthest_dist = dist;
+      farthest_angle = angle[i];
+    }    
+  }
+
+  if (farthest_dist < 20) {
+    reverse();
+  } else {
+    drive(farthest_angle);
+  }
+
+};
+
+void drive( int drive_angle){
+  
+  if ((drive_angle-90) < 0){
+    //turn left
+    go(RIGHT,255);
+    go(LEFT,(1-(90-drive_angle)/90)*255);
+  } else if ((drive_angle-90) > 0){
+    //turn right
+    go(LEFT,255);
+    go(RIGHT,(1-(drive_angle-90)/90)*255);
+  } else {
+    //go straight
+    go(RIGHT,255);
+    go(LEFT,255);
   }
 }
-
-
 
 
--- code/arduino-vehicle-serial/arduino-vehicle-serial.ino
+++ code/arduino-vehicle-serial/arduino-vehicle-serial.ino
@@ -2,13 +2,17 @@
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 
-
-#define PWM_MIN 300
+// #define LED_BUILTIN 2 // Remapping the built-in LED since the NodeMcu apparently uses a different one.
+// #define LED_BUILTIN_RED 16 // If using a NodeMcu v1, then there's another red onboard led.
+// The min amount of PWM the motors need to move. Depends on the battery, motors and controller.
+// The max amount is defined by PWMRANGE in Arduino.h
+#define PWM_MIN 0
+#define PWMRANGE 255
 
 // Declare functions
 void setupPins();
 void setupSerial();
-bool rosConnected();
+// bool rosConnected();
 void onTwist(const geometry_msgs::Twist &msg);
 float mapPwm(float x, float out_min, float out_max);
 
@@ -23,26 +27,30 @@ const uint8_t L_PWM = 6;
 
 // ROS serial server
 ros::NodeHandle node;
-ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &onTwist);
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &onTwist);
 
 bool _connected = false;
 
 void setup()
 {
   setupPins();
-  setupSerial();
+  //setupSerial();
   
   // Connect to rosserial socket server and init node. (Using default port of 11411)
-  //node.getHardware()->setConnection(server);
+  node.getHardware()->setBaud(57600);
   node.initNode();
   node.subscribe(sub);
+
+  // node.logerror(printf ("Connected: %s", node.connected() ? "true":"false"));
+
+  node.logerror("log: Subscribed");  
 }
 
 void setupPins()
 {
   // Status LED
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  // pinMode(LED_BUILTIN, OUTPUT);
+  // digitalWrite(LED_BUILTIN, HIGH);
 
   pinMode(L_PWM, OUTPUT);
   pinMode(L_FORW, OUTPUT);
@@ -56,12 +64,13 @@ void setupPins()
 void setupSerial()
 {
   Serial.begin(57600);
-  Serial.println();
+  Serial.println("arduino_vehicle - Serial Connected");
 }
 
 
 void stop()
 {
+  // node.logerror("72: stopped");
   digitalWrite(L_FORW, 0);
   digitalWrite(L_BACK, 0);
   digitalWrite(R_FORW, 0);
@@ -70,9 +79,15 @@ void stop()
   analogWrite(R_PWM, 0);
 }
 
-void onTwist(const geometry_msgs::Twist &msg)
-{
-    // Cap values at [-1 .. 1]
+void onTwist(const geometry_msgs::Twist& msg)
+{ 
+  // if (!_connected){
+  //   stop();
+  //   node.logerror("ln81: Not connected");
+  //   return;
+  // }
+  
+  // Cap values at [-1 .. 1]
   float x = max(min(msg.linear.x, 1.0f), -1.0f);
   float z = max(min(msg.angular.z, 1.0f), -1.0f);
 
@@ -80,10 +95,10 @@ void onTwist(const geometry_msgs::Twist &msg)
   // Taken from https://hackernoon.com/unicycle-to-differential-drive-courseras-control-of-mobile-robots-with-ros-and-rosbots-part-2-6d27d15f2010#1e59
   float l = (msg.linear.x - msg.angular.z) / 2;
   float r = (msg.linear.x + msg.angular.z) / 2;
-
+  // node.logerror(printf("l = %f | r = %f", l, r));
   // Then map those values to PWM intensities. PWMRANGE = full speed, while PWM_MIN = the minimal amount of power at which the motors begin moving.
-  uint16_t lPwm = mapPwm(fabs(l), 0, 255*2);
-  uint16_t rPwm = mapPwm(fabs(r), 0, 510);
+  uint16_t lPwm = mapPwm(fabs(l), PWM_MIN, PWMRANGE);
+  uint16_t rPwm = mapPwm(fabs(r), PWM_MIN, PWMRANGE);
 
   // Set direction pins and PWM
   digitalWrite(L_FORW, l > 0);
@@ -96,24 +111,30 @@ void onTwist(const geometry_msgs::Twist &msg)
 
 void loop()
 {
-   node.spinOnce();
+  // if (!rosConnected())
+  //   stop();
+  node.spinOnce();
+  delay(10);
 }
 
-// bool rosConnected()
-// {
-//   // If value changes, notify via LED and console.
-//   bool connected = node.connected();
-//   if (_connected != connected)
-//   {
-//     _connected = connected;
-//     digitalWrite(LED_BUILTIN, !connected); // false -> on, true -> off
-//     Serial.println(connected ? "ROS connected" : "ROS disconnected");
-//   }
-//   return connected;
-// }
+bool rosConnected()
+{
+  // If value changes, notify via LED and console.
+  bool connected = node.connected();
+  if (_connected != connected)
+  {
+    _connected = connected;
+    // node.logerror(connected ? "ROS connected" : "ROS disconnected")   ; 
+    // digitalWrite(LED_BUILTIN, !connected); // false -> on, true -> off
+    Serial.println(connected ? "ROS connected" : "ROS disconnected");
+  }
+  return connected;
+}
 
 // Map x value from [0 .. 1] to [out_min .. out_max]
 float mapPwm(float x, float out_min, float out_max)
 {
-  return x * (out_max - out_min) + out_min;
+  float y = x * (out_max - out_min) + out_min;
+  // node.logerror(printf(" Max pwm: %f", y))  ;
+  return y;
 }
